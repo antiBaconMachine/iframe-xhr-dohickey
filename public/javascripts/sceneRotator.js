f4d3c434aeac720909a759b42979b79581c964e2
Ollie Edwards <ollie@brandwatch.com>
2017-01-23 11:07:42 +0000

    neaten up scenerotator

--- public/javascripts/sceneRotator.js
+++ public/javascripts/sceneRotator.js
@@ -1,24 +1,24 @@
 window.onload = function () {
   const iframe = document.querySelector('iframe')
-  const urlParams = new URLSearchParams(window.location.search)
-  const h2 = document.querySelector('h2');
-
-  let scene = urlParams.has('scene') ? urlParams.get('scene') : 1
-
-  function update() {
-    iframe.src = `/scenes/${scene}`
-    h2.innerHTML = `Scene: ${scene}`
-  }
-
-  update()
 
   if (iframe) {
-    iframe.src = `/scenes/${scene}`
-    setInterval(() => {
+    const urlParams = new URLSearchParams(window.location.search)
+    const h2 = document.querySelector('h2');
+
+    let scene = urlParams.has('scene') ? urlParams.get('scene') : 1
+
+    function update() {
+      iframe.src = `/scenes/${scene}`
+      h2.innerHTML = `Scene: ${scene}`
+    }
+
+    update()
+
+    return setInterval(() => {
       scene = scene === 1 ? 2 : 1
       update()
     }, 1000)
-  } else {
-    setInterval(() => window.location = `/single`, 1000)
   }
+
+  setInterval(() => window.location = `/single`, 1000)
 }
